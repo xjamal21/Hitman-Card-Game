24177cc5492a2d99662204c4bd55e8401bcbfa2b
xjamal21 <jayz445hood@gmail.com>
2026-05-15 11:09:26 +0800

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,36 @@
+# add introduction
+import time
+import random
+import string
+import os
+
+chars = string.ascii_letters + string.digits + "/\{}[]<>"
+
+for i in range(50):
+    loading = "".join(random.choice (chars) for i in range (50))
+    print(loading)
+    time.sleep(0.08)
+
+os.system("cls")
+
+player_list = []
+
+
+while True:
+    name_input = input("Insert your name (type 'start' to start the game): ")
+
+    if name_input != "start":
+        player_list.append(name_input)
+    else:
+        if len(player_list) < 2:
+            print("Not enough players!")
+        else:
+            break
+
+print("=========================== PLAYERS ===============================")
+
+print(f" > {player_list}")
+
+print("===================================================================")
+
+
---
+++ cards.py
@@ -0,0 +1,22 @@
+cards = {
+    "hitman": {
+        "name": "Hitman",
+        "desc": "You're dead.",
+        "num": 0
+    },
+    "angel": {
+        "name": "Angel Card",
+        "desc": "It saves you from Hitman.",
+        "num": 0
+    },
+    "skip": {
+        "name": "Skip Card",
+        "desc": "Skip your turn.",
+        "num": 0
+    },
+    "hitman ": {
+        "name": "Hitman",
+        "desc": "You're dead.",
+        "num": 0
+    },
+}
