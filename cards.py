7f932992983a0be5dae78589b7351f397b26d976
elementsgun <chujinfong@gmail.com>
2026-05-16 22:52:16 +0800

    Add the rest cards and desc

--- cards.py
+++ cards.py
@@ -6,17 +6,52 @@ cards = {
     },
     "angel": {
         "name": "Angel Card",
-        "desc": "It saves you from Hitman.",
+        "desc": "Protects you when you draw the Hitman.",
         "num": 0
     },
     "skip": {
         "name": "Skip Card",
-        "desc": "Skip your turn.",
+        "desc": "End your turn without drawing a card.",
         "num": 0
     },
-    "hitman ": {
-        "name": "Hitman",
-        "desc": "You're dead.",
+    "future": {
+        "name": "Future Card",
+        "desc": "secretly view the top 3 cards in the deck.",
         "num": 0
     },
+    "reverse": {
+        "name": "Reverse Card",
+        "desc": "End your turn and reverse the play order.",
+        "num": 0
+    },
+    "attack": {
+        "name": "Attack Card",
+        "desc": "Skip your turn. A player you choose must take 2 turns in his next turn.",
+        "num": 0
+    },
+    "steal": {
+        "name": "Steal Card",
+        "desc": "Choose a player and steal a random card from his hand.",
+        "num": 0
+    },
+    "mirror": {
+        "name": "Mirror Card",
+        "desc": "Copy the effect of the card underneath.",
+        "num": 0
+    },
+    "shuffle": {
+        "name": "Shuffle Card",
+        "desc": "Shuffle the deck.",
+        "num": 0
+    },
+    "inferno": {
+        "name": "Inferno Card",
+        "desc": "Remove the last card copies from all players hands and put them in the deck.",
+        "num": 0
+    },
+    "bottom": {
+        "name": "Bottom Card",
+        "desc": "End your turn by drawing from the bottom of the deck.",
+        "num": 0
+    }
 }
