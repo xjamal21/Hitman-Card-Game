eac76def7be93e018de8654dd024b6ddc24f862b
elementsgun <chujinfong@gmail.com>
2026-05-19 21:41:22 +0800

    finish main game mechanic

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 # add introduction
 # dixon add menu with start setting-language quit and tutorial
+# dixon add player alive display, add chances of getting a hitman
+
 import time
 import random
 import string
@@ -21,16 +23,17 @@ os.system("cls")
 current_game = Game()
 
 # add player into the list
+# try make this to a class method
 while True:
     name_input = input("Insert your name (type 'start' to start the game): ")
 
     if name_input == "start":
-        if current_game.player_count() < 2:
+        if current_game.get_player_count() < 2:
             print("Not enough players!")
         else:
             break
     else:
-        if current_game.player_count() >= 6:
+        if current_game.get_player_count() >= 6:
             print("Too many players. Insert 'start' to start the game.")
         else:
             current_game.add_player(name_input)
@@ -42,8 +45,10 @@ print("===================================================================")
 
 # construct the deck based on player count
 game_deck = Deck()
-game_deck.build_deck(current_game.player_count())
+game_deck.build_deck(current_game.get_player_count())
 
 # setup game
 current_game.setup_game(game_deck.decks)
-print(game_deck.decks)
+
+# start game
+current_game.start_game_loop(game_deck.decks)
--- game.py
+++ game.py
@@ -11,7 +11,7 @@ class Game:
     def add_player(self, name):
         self.players.append(Player(name))
         
-    def player_count(self):
+    def get_player_count(self):
         return len(self.players)
     
     def setup_game(self, deck):
@@ -21,9 +21,54 @@ class Game:
 
             for _ in range(4):
                 for card in deck: #skip hitman and proceed to next card
-                    if card != "Hitman":
+                    if card.name != "Hitman":
                         deck.remove(card)
                         player.hand.append(card)
-                        print(player.hand)
                         break
-        # smth like setup complete everyone has 5 cards
+        # dixon send a msg like setup complete everyone has 5 cards
+        
+    def start_game_loop(self, deck):
+        game_over = False
+        
+        print("Game start.")
+        
+        while not game_over:
+            for current_player in self.players:
+                # skip dead players
+                if not current_player.isAlive:  
+                    continue
+                
+                print(f"Now is {current_player.name} turn.")
+                
+                required_turns = getattr(current_player, "turn")
+                turn = 0
+                
+                # loop turn until player finish required turns
+                while turn < required_turns:
+                    print(f"Turn {turn + 1} of total {required_turns} turns.")
+                    current_player.show_hand()
+                    player_choice = input("Take an action. (p) Play a card (d) Draw a card: ")
+                    
+                    if player_choice == "p":
+                        played_card = current_player.play_card()
+                        if played_card is not None:
+                            # end 1 turn if the card played does skip turn
+                            if played_card.doesSkipTurn:
+                                turn += 1
+                    elif player_choice == "d":
+                        current_player.draw_card(deck)
+                        turn += 1
+                    else:
+                        print("Invalid input. Type 'p' or 'd'.")
+            
+                # detect numbers of players alive
+                alive_players = []     
+                for player in self.players:
+                    if player.isAlive:
+                        alive_players.append(player)
+                
+                # end the game if alive players is 1
+                if len(alive_players) == 1:
+                    print(f"GAME OVER! {alive_players[0]} is the winner!") 
+                    game_over = True
+                    break          
